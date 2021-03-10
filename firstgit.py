43bc79bf0aeef5073230b333abaa2501ddb2626f
Philip Britton <philipbritton3@gmail.com>
2021-03-10 01:57:06 +0000

    #testing graphs

--- firstgit.py
+++ firstgit.py
@@ -1,3 +1,13 @@
-name = "Philip"
+import matplotlib.pyplot as plt
+import numpy as np
+import pandas as pd
+import pandas as pd
+import seaborn as sns
+df = pd.read_csv("updated_listings5.csv")
+sns.catplot(x="region_parent_name", kind="count", data=df)
+plt.xticks(rotation=90)
+plt.show()
 
-x  = [1,2,3,4]
+
+
+
